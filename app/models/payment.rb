60953dafd9fd577f91447aae9dea766bb2130d36
nirbelelti <nirbelelti@gmail.com>
2024-04-18 14:52:00 +0200

    Rspec: Payment model add validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,8 @@
 class Payment < ApplicationRecord
   belongs_to :organisation
 
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :sender_id, presence: true
+  validates :receiver_id, presence: true
+
 end
