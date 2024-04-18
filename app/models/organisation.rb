d0c42440625414f3d9c6b7e88aa252340153916b
nirbelelti <nirbelelti@gmail.com>
2024-04-18 14:49:32 +0200

    Organisation add validation and association

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,11 @@
 class Organisation < ApplicationRecord
+  has_many :payments
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2}
+  validates :address, presence: true, length: { minimum: 2}
+  validates :vat_id, presence: true, length: { minimum: 2}
+  validates :crm_id, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :segment, presence: true
 
 end
