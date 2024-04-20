303aad3e5e50e7b22ed7cba4d67f0e418eda5e8a
nirbelelti <nirbelelti@gmail.com>
2024-04-20 19:22:45 +0200

    Add payments_controller.rb controller using a facade and supporting api/v1 requests structure

---
+++ app/controllers/api/v1/payments_controller.rb
@@ -0,0 +1,45 @@
+require_dependency 'payment_facade'
+
+
+class Api::V1::PaymentsController < ApplicationController
+  before_action :set_payment, only: %i[ show update destroy ]
+
+  def index
+    payments = PaymentFacade.all_payments
+    render json: payments
+  end
+
+  def show
+    render json: @payment
+  end
+
+  def create
+    payment = PaymentFacade.create_payment(payment_params)
+    render json: payment, status: :created
+  end
+
+  def update
+    if PaymentFacade.update_payment(@payment, payment_params)
+      render json: @payment
+    else
+      render json: @payment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    PaymentFacade.delete_payment(@payment)
+  end
+
+  private
+
+  def set_payment
+    @payment = PaymentFacade.find_payment(params[:id])
+  end
+
+  def payment_params
+    params.require(:payment).permit(:organisation_id,
+                                    :sender_id,
+                                    :receiver_id,
+                                    :amount)
+  end
+end
