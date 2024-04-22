f72044050b8c34402b72f396eb84d7b3d404100a
nirbelelti <nirbelelti@gmail.com>
2024-04-22 15:55:08 +0200

    Rspec: - add tests to scope return last three payments

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Organisation, type: :model do
+  let!(:organisation) { FactoryBot.create(:organisation) }
 
   describe 'Validations' do
     it { should validate_presence_of(:name) }
@@ -17,5 +18,34 @@ RSpec.describe Organisation, type: :model do
 
   describe 'Associations' do
     it { should have_many(:payments) }
+  end
+
+  describe 'Scopes' do
+
+    describe 'with_last_three_payments' do
+
+      4.times do |i|
+        let!("payment#{i + 1}") { FactoryBot.create(:payment, created_at: i.days.ago, organisation: organisation) }
+      end
+
+      it 'returns organisations with their last three payments' do
+        result = Organisation.with_last_three_payments
+        expect(result.size).to eq(1)
+
+        organisation_data = result.first
+        expect(organisation_data[:id]).to eq(organisation.id)
+        expect(organisation_data[:name]).to eq(organisation.name)
+        expect(organisation_data[:address]).to eq(organisation.address)
+
+        last_payments = organisation_data[:last_payments]
+        expect(last_payments.size).to eq(3)
+        expect(last_payments).to include(
+                                   { date: payment1.created_at.strftime('%m/%d/%Y'), amount: payment1.amount, sender: payment1.sender_id, receiver: payment1.receiver_id },
+                                   { date: payment2.created_at.strftime('%m/%d/%Y'), amount: payment2.amount, sender: payment2.sender_id, receiver: payment2.receiver_id },
+                                   { date: payment3.created_at.strftime('%m/%d/%Y'), amount: payment3.amount, sender: payment3.sender_id, receiver: payment3.receiver_id }
+                                 )
+        expect(last_payments).not_to include({ date: payment4.created_at.strftime('%m/%d/%Y'), amount: payment4.amount, sender: payment4.sender_id, receiver: payment4.receiver_id })
+      end
+    end
   end
 end
