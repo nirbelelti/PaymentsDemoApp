ecacdf95e3ddbc698709a566aa932077d3acf299
nirbelelti <nirbelelti@gmail.com>
2024-04-22 16:26:19 +0200

    Add scope to organisation return last three payments

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,25 @@
 class Organisation < ApplicationRecord
   has_many :payments
 
+  scope :with_last_three_payments, -> {
+    organisations_with_payments = includes(:payments).to_a
+
+    organisations_with_payments.map do |org|
+      last_three_payments = org.payments.order(created_at: :desc).limit(3)
+      last_three_payments_hash = last_three_payments.map do |payment|
+        { date: payment.created_at.strftime('%m/%d/%Y'), amount: payment.amount, sender: payment.sender_id, receiver: payment.receiver_id }
+      end
+
+      {
+        id: org.id,
+        name: org.name,
+        address: org.address,
+        crm_id: org.crm_id,
+        last_payments: last_three_payments_hash.presence || 'No Payments'
+      }
+    end
+  }
+
   validates :name, presence: true, uniqueness: true, length: { minimum: 2}
   validates :address, presence: true, length: { minimum: 2}
   validates :vat_id, presence: true, length: { minimum: 2}
