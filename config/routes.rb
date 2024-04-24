f74b958fc6e49171fdc138a779f0d5fc35beb5bf
nirbelelti <nirbelelti@gmail.com>
2024-04-24 18:02:46 +0200

    Add route for organisation transfer payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :organisations
+      resources :organisations do
+        post :transfer_payment, on: :member
+      end
       resources :payments
     end
   end
