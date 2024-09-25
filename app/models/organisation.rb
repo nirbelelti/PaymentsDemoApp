ecd9dedb83f8be053a74b32b2a31961a3149566b
nirbelelti <nirbelelti@gmail.com>
2024-09-25 12:59:21 +0200

    organization.rb: Modify with_last_three_payments scope to use subquery for improved robustness and performance.(The previous version used SQL light which it can not support subqueries) remove redundant validation add uuid validation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,30 +3,26 @@ class Organisation < ApplicationRecord
   has_many :payments,  foreign_key: :receiver_id, dependent: :destroy
 
   scope :with_last_three_payments, -> {
-    organisations_with_payments = includes(:payments).to_a
+    subquery = Payment.select('payments.*, ROW_NUMBER() OVER (PARTITION BY sender_id ORDER BY payments.created_at DESC) AS row_num')
+                      .to_sql
 
-    organisations_with_payments.map do |org|
-      last_three_payments = org.payments.order(created_at: :desc).limit(3)
-      last_three_payments_hash = last_three_payments.map do |payment|
-        { date: payment.created_at.strftime('%m/%d/%Y'), amount: payment.amount, sender: payment.sender_id, receiver: payment.receiver_id }
-      end
-
-      {
-        id: org.id,
-        name: org.name,
-        address: org.address,
-        crm_id: org.crm_id,
-        segment: org.segment,
-        last_payments: last_three_payments_hash.presence || 'No Payments'
-      }
-    end
+    joins("LEFT JOIN (#{subquery}) AS last_payments ON organisations.id = last_payments.sender_id")
+      .where('last_payments.row_num <= 3')
+      .select("organisations.*,
+            ARRAY_AGG(json_build_object(
+              'id', last_payments.id,
+              'created_at', last_payments.created_at,
+              'amount', last_payments.amount,
+              'receiver_id', last_payments.receiver_id
+            ) ORDER BY last_payments.created_at DESC) AS last_three_payments")
+      .group('organisations.id')
   }
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 2}
   validates :address, presence: true, length: { minimum: 2}
   validates :vat_id, presence: true, length: { minimum: 2}
-  validates :crm_id, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :segment, presence: true
+  validates :uuid, presence: true, uniqueness: true
 
 end
