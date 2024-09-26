cbecad81dc03df39f69f8bca2649312d72fca20d
nirbelelti <nirbelelti@gmail.com>
2024-09-26 11:34:16 +0200

    Update organisation factory

--- spec/factories/organisation.rb
+++ spec/factories/organisation.rb
@@ -3,11 +3,12 @@ FactoryBot.define do
     name { Faker::Company.unique.name }
     address { Faker::Address.street_address }
     vat_id { Faker::Number.unique.number(digits: 10) }
-    crm_id { Faker::Number.unique.number(digits: 10) }
     email { Faker::Internet.unique.email }
     segment { Faker::Commerce.department }
     country { Faker::Address.country }
     province { Faker::Address.state }
     zip { Faker::Address.zip_code }
+    uuid { SecureRandom.uuid }
+    balance { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
   end
 end
