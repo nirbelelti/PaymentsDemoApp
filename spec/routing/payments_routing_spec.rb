f5b06d1fc5543abc6907f5e0aa5e6fe000c1c57b
nirbelelti <nirbelelti@gmail.com>
2024-09-26 11:35:46 +0200

    Update routing tests

--- spec/routing/payments_routing_spec.rb
+++ spec/routing/payments_routing_spec.rb
@@ -15,16 +15,9 @@ RSpec.describe Api::V1::PaymentsController, type: :routing do
       expect(post: '/api/v1/payments').to route_to('api/v1/payments#create')
     end
 
-    it 'routes to #update via PUT' do
-      expect(put: '/api/v1/payments/1').to route_to('api/v1/payments#update', id: '1')
+    it 'routes to #refund' do
+      expect(post: '/api/v1/payments/1/refund').to route_to('api/v1/payments#refund', id: '1')
     end
 
-    it 'routes to #update via PATCH' do
-      expect(patch: '/api/v1/payments/1').to route_to('api/v1/payments#update', id: '1')
-    end
-
-    it 'routes to #destroy' do
-      expect(delete: '/api/v1/payments/1').to route_to('api/v1/payments#destroy', id: '1')
-    end
   end
 end
