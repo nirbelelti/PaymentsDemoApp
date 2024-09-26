bcd96a2737a2fc90769a22b7986d18a5a5d16a08
nirbelelti <nirbelelti@gmail.com>
2024-09-26 11:33:37 +0200

    Add foreign keys for payments

--- db/migrate/20240923133107_create_payments.rb
+++ db/migrate/20240923133107_create_payments.rb
@@ -1,9 +1,9 @@
 class CreatePayments < ActiveRecord::Migration[7.1]
   def change
     create_table :payments do |t|
-      t.references :vendor, null: false
-      t.references :sender, null: false
-      t.references :receiver, null: false
+      t.references :vendor, null: false, foreign_key: {to_table: :vendors}
+      t.references :sender, null: false, foreign_key: { to_table: :organisations }
+      t.references :receiver, null: false, foreign_key: { to_table: :organisations }
       t.decimal :amount, null: false, precision: 10, scale: 2
       t.integer :status, default: 0, null: false
 
